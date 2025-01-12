eb8caf137d47a9ba792ee1d1de15526ba77bc3f3
IrfanabbasIS <155755533+IrfanabbasIS@users.noreply.github.com>
2025-01-12 21:46:38 +0530

    Update Login.js

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -10,7 +10,7 @@ const navigate = useNavigate();
 
 const handleSubmit=async(e)=>{
     e.preventDefault();
-    const response= await fetch("http://localhost:5000/api/loginuser", {
+    const response= await fetch("https://full-stack-task-management-2.onrender.com", {
         method:"POST",
         headers:{
             "Content-Type":"application/json"
