5934fc8b650654cd49b381b0e2f7308caf3d96c7
IrfanabbasIS <155755533+IrfanabbasIS@users.noreply.github.com>
2025-01-12 21:47:08 +0530

    Update Home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -8,7 +8,7 @@ export default function Home() {
     const [foodItem, setFoodItem] = useState([]);
 
     const loadData = async () => {
-        let response = await fetch("http://localhost:5000/api/foodData", {
+        let response = await fetch("https://full-stack-task-management-2.onrender.com", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
